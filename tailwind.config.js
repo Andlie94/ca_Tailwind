4072ac3bababa5b10d78a95f3ae9ff9a28569507
Andlie94 <andlie02174@stud.noroff.no>
2025-03-27 16:30:58 +0100

    made the loginpage form and styled it, made buttons in the inputfelt. added color og fonts in config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}","!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0747A7', //
+        secondary: '#E9A876',
+        element: '#E9A876',
+        background: '#FFFDF2',
+        text: '#212529',
+        text2: '#ffffff',
+      },
+      fontFamily: {
+        'heading': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
